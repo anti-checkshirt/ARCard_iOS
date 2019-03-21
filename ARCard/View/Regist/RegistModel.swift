b59c869c69bd4b86eeeaee9291cef9ad9e3c37c5
tomoki69386 <tomoki_69386@yahoo.co.jp>
2019-03-21 22:34:44 +0900

    Add Regist作成

---
+++ ARCard/View/Regist/RegistModel.swift
@@ -0,0 +1,9 @@
+//
+//  RegistModel.swift
+//  ARCard
+//
+//  Created by 築山朋紀 on 2019/03/21.
+//  Copyright © 2019 tomoki. All rights reserved.
+//
+
+import Foundation
---
+++ ARCard/View/Regist/RegistViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RegistViewController.swift
+//  ARCard
+//
+//  Created by 築山朋紀 on 2019/03/21.
+//  Copyright © 2019 tomoki. All rights reserved.
+//
+
+import UIKit
+
+class RegistViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
