0f06fcd6eaae377c7393fed6769d3c097288aeb5
tomoki69386 <tomoki_69386@yahoo.co.jp>
2019-03-22 14:20:50 +0900

    Update

--- ARCard/View/SettingTableViewController.swift
+++ ARCard/View/SettingTableViewController.swift
@@ -20,6 +20,49 @@ class SettingTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print(indexPath.row)
+        switch indexPath.section {
+        case 0:
+            switch indexPath.row {
+            case 0:
+                print("プロフィール編集")
+            case 1:
+                print("ログアウト")
+            case 2:
+                print("退会")
+            default:
+                break
+            }
+        case 1:
+            switch indexPath.row {
+            case 0:
+                print("AR名刺を応援する")
+            case 1:
+                print("不具合を報告する")
+            case 2:
+                print("お問い合わせ")
+            case 3:
+                print("アプリの使い方")
+            default:
+                break
+            }
+        case 2:
+            switch indexPath.row {
+            case 0:
+                print("利用規約")
+            case 1:
+                print("プライバシーポリシー")
+            default:
+                break
+            }
+        case 3:
+            switch indexPath.row {
+            case 0:
+                print("バージョン")
+            default:
+                break
+            }
+        default:
+            break
+        }
     }
 }
