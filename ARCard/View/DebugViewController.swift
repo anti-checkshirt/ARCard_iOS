e51677d87da4d6d3f6f26aa2ac7ed656948f992d
tomoki69386 <tomoki_69386@yahoo.co.jp>
2019-03-22 16:55:51 +0900

    Update Debug

---
+++ ARCard/View/DebugViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DebugViewController.swift
+//  ARCard
+//
+//  Created by 築山朋紀 on 2019/03/22.
+//  Copyright © 2019 tomoki. All rights reserved.
+//
+
+import UIKit
+
+class DebugViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
