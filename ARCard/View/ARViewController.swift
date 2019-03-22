0b6900cdcc53635015f5132dd4cbafea3d9c5310
tomoki69386 <tomoki_69386@yahoo.co.jp>
2019-03-22 14:01:20 +0900

    Add Setting

--- ARCard/View/ARViewController.swift
+++ ARCard/View/ARViewController.swift
@@ -7,8 +7,12 @@
 //
 
 import UIKit
+import ARKit
+import SceneKit
 
 class ARViewController: UIViewController {
+    
+    @IBOutlet private weak var sceneView: ARSCNView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,15 +20,7 @@ class ARViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override var prefersStatusBarHidden: Bool {
+        return true
     }
-    */
-
 }
