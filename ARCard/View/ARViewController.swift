4342f87d516499d4fccf308f1c01903b3ff8cdfd
tomoki69386 <tomoki_69386@yahoo.co.jp>
2019-03-22 13:46:07 +0900

    Add AR

---
+++ ARCard/View/ARViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ARViewController.swift
+//  ARCard
+//
+//  Created by 築山朋紀 on 2019/03/22.
+//  Copyright © 2019 tomoki. All rights reserved.
+//
+
+import UIKit
+
+class ARViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
