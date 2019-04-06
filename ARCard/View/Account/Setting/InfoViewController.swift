6d2dd9b47d973784256d33d497725f86d32daa8e
tomoki69386 <tomoki_69386@yahoo.co.jp>
2019-04-07 03:05:03 +0900

    Create Info

---
+++ ARCard/View/Account/Setting/InfoViewController.swift
@@ -0,0 +1,17 @@
+//
+//  InfoViewController.swift
+//  ARCard
+//
+//  Created by 築山朋紀 on 2019/04/07.
+//  Copyright © 2019 tomoki. All rights reserved.
+//
+
+import UIKit
+
+class InfoViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
