62b71412dc1403538f96429e9d5e5cd77d3562d6
tomoki69386 <tomoki_69386@yahoo.co.jp>
2019-04-07 05:15:57 +0900

    Add color

--- ARCard/App/AppDelegate.swift
+++ ARCard/App/AppDelegate.swift
@@ -17,6 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        UINavigationBar.appearance().barTintColor = UIColor(named: "main")
+        UINavigationBar.appearance().tintColor = .white
+        UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
         return true
     }
 
