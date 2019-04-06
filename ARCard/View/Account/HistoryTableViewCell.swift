75ae4ba35b3d59aca72736c920532d53b21e4cf0
tomoki69386 <tomoki_69386@yahoo.co.jp>
2019-04-07 04:52:15 +0900

    Create History

---
+++ ARCard/View/Account/HistoryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HistoryTableViewCell.swift
+//  ARCard
+//
+//  Created by 築山朋紀 on 2019/04/07.
+//  Copyright © 2019 tomoki. All rights reserved.
+//
+
+import UIKit
+
+class HistoryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
